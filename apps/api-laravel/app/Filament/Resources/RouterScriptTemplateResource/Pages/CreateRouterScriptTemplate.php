c4e12ddcb2c9b929dd46055dbdd86c6baf3e2eba
ghonnn <ghonnn@gmail.com>
2026-06-20 03:23:25 +0700

    feat: add router script template create page

---
+++ apps/api-laravel/app/Filament/Resources/RouterScriptTemplateResource/Pages/CreateRouterScriptTemplate.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\RouterScriptTemplateResource\Pages;
+
+use App\Filament\Resources\RouterScriptTemplateResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateRouterScriptTemplate extends CreateRecord
+{
+    protected static string $resource = RouterScriptTemplateResource::class;
+}
